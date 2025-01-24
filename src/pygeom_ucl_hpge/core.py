e3c064eda8514e9103a46f30912c5be4f11cdf92
Toby Dixon <tdixon@berkeley.edu>
2025-01-24 13:15:20 +0100

    [tests] add a basic test

--- src/pygeom_ucl_hpge/core.py
+++ src/pygeom_ucl_hpge/core.py
@@ -1,1 +1,5 @@
 from __future__ import annotations
+
+
+def construct_hpge(metadata: dict):
+    raise NotImplementedError
--- tests/test_package.py
+++ tests/test_package.py
@@ -1,1 +1,10 @@
 from __future__ import annotations
+
+import pytest
+
+from pygeom_ucl_hpge.core import construct_hpge
+
+
+def test_import():
+    with pytest.raises(NotImplementedError):
+        construct_hpge({})
